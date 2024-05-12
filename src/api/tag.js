a70bb7fc0094a751f29f94e8668010f97e73ea1f
MingJie Li <kadsg0769@163.com>
2024-05-12 09:34:33 +0800

    feature: 实现标签管理功能

---
+++ src/api/tag.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 分页获取标签列表
+export function queryNewsTag(data) {
+  return request({
+    url: '/newsTag/query',
+    method: 'post',
+    data
+  })
+}
+
+// 获取所有标签
+export function getAllNewsTag() {
+  return request({
+    url: '/newsTag/list',
+    method: 'get'
+  })
+}
+
+// 获取标签详情
+export function getNewsTag(id) {
+  return request({
+    url: '/newsTag/' + id,
+    method: 'get'
+  })
+}
+
+// 添加标签
+export function addNewsTag(data) {
+  return request({
+    url: '/newsTag',
+    method: 'post',
+    data
+  })
+}
+
+// 更新标签
+export function updateNewsTag(data) {
+  return request({
+    url: '/newsTag',
+    method: 'put',
+    data
+  })
+}
--- src/router/index.js
+++ src/router/index.js
@@ -49,102 +49,35 @@ export const constantRoutes = [
     redirect: '/dashboard',
     children: [{
       path: 'dashboard',
-      name: 'Dashboard',
+      name: '首页',
       component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
+      meta: { title: '首页', icon: 'dashboard' }
     }]
   },
 
   {
-    path: '/example',
+    path: '/management',
     component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'el-icon-s-help' },
+    name: 'Management',
+    meta: { title: '系统管理', icon: 'el-icon-menu' },
     children: [
       {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
+        path: 'tag',
+        name: 'Tag',
+        component: () => import('@/views/management/tag/index'),
+        meta: { title: '标签管理', icon: 'el-icon-notebook-1' }
       },
       {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
-  },
-
-  {
-    path: '/form',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
-  },
-
-  {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
-    children: [
-      {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
+        path: 'role',
+        name: 'Role',
+        component: () => import('@/views/management/role/index'),
+        meta: { title: '角色管理', icon: 'form' }
       },
       {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        name: 'Menu2',
-        meta: { title: 'menu2' }
+        path: 'user',
+        name: 'User',
+        component: () => import('@/views/management/user/index'),
+        meta: { title: '用户管理', icon: 'form' }
       }
     ]
   },
